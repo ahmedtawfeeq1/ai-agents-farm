33392c96d7c71c0b69440167ed0245eeea9417aa
ahmedtawfeeq1 <ahmed.tawfeeq@ejust.edu.eg>
2024-12-16 03:04:22 +0200

    Updates

--- assets/contact-DM7DDCny.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-C2SBhqyQ.js";const r=()=>t.jsx("h1",{children:"Contact Us"});export{r as default};
--- assets/list-BpKFR-V0.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-C2SBhqyQ.js";const e=()=>t.jsx("h1",{children:"List of Agents"});export{e as default};
